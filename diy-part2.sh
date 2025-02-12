b7724743cf1c1f60c53948e80d774714a00d8fcb
082530 <38971082+082530@users.noreply.github.com>
2025-02-12 10:44:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,5 +24,15 @@ sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/luci/applications/alist
+rm -rf feeds/luci/applications/luci-app-diskman
+rm -rf feeds/luci/applications/luci-app-dockerman
+rm -rf feeds/luci/applications/luci-app-filetransfer
+rm -rf feeds/luci/applications/luci-app-samba4
+rm -rf feeds/luci/applications/luci-app-rclone
+rm -rf feeds/luci/applications/luci-app-turboacc
+rm -rf feeds/luci/applications/luci-app-vlmcsd
+get clone https://github.com/sirpdboy/luci-app-eqosplus.git feeds/luci/applications/
 ./scripts/feeds update -a
 ./scripts/feeds install -a
