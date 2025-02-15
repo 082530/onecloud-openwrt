537549e361696d6ca0852995579d6c2afe2cfa29
082530 <38971082+082530@users.noreply.github.com>
2025-02-15 18:00:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,25 +13,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.254/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
+# Modify default hostname
 sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
 
 # 替换终端为bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
+# 清理不需要的包并确保需要的包被正确安装
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/luci/applications/luci-app-alist
@@ -47,26 +35,19 @@ rm -rf feeds/luci/applications/luci-app-turboacc
 rm -rf feeds/NueXini_Packages/luci-app-istoreenhance
 rm -rf feeds/NueXini_Packages/luci-app-xunlei
 rm -rf feeds/luci/applications/luci-app-vlmcsd
+
+# 拉取需要的额外包
 git clone https://github.com/sirpdboy/luci-app-eqosplus.git feeds/luci/applications/luci-app-eqosplus
 git clone https://github.com/kiddin9/kwrt-packages
+
+# 移动需要的包到正确位置
 cd kwrt-packages && mv luci-app-chinadns-ng luci-app-cpu-perf vmease openwrt-natmapt natter2 autoshare-samba luci-app-dockerman luci-app-natmapt luci-app-natter2 luci-app-onliner luci-app-rtbwmon luci-app-frpc luci-app-istoredup luci-app-istoreenhance luci-app-istorego luci-app-istorepanel luci-app-rclone luci-app-samba4 luci-app-turboacc luci-app-wolplus luci-app-xunlei ../feeds/luci/applications/
 cd ../ && rm -rf kwrt-packages
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-chinadns-ng
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-cpu-perf
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-dockerman
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-natmapt
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-natter2
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-onliner
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-rtbwmon
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-frpc
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-istoredup
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-istoreenhance
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-istorego
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-istorepanel
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-rclone
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-samba4
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-turboacc
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-wolplus
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-xunlei
+
+# 更新和安装feeds，确保包被正确处理
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+# 重新运行 make menuconfig 以确保这些包已经被选中
+# 编译前确保选中的包都已启用，更新 .config
+make defconfig
